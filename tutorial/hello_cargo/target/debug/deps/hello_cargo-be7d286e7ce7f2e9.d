0be19a1d97a948fbf64dc0913a5871f063609d40
Ian McNaney <ianmcnaney@Ians-MacBook-Air.local>
2022-08-31 21:39:56 -0600

    tutorial start

---
+++ tutorial/hello_cargo/target/debug/deps/hello_cargo-be7d286e7ce7f2e9.d
@@ -0,0 +1,5 @@
+/Users/ianmcnaney/src/rustystuff/tutorial/hello_cargo/target/debug/deps/hello_cargo-be7d286e7ce7f2e9: src/main.rs
+
+/Users/ianmcnaney/src/rustystuff/tutorial/hello_cargo/target/debug/deps/hello_cargo-be7d286e7ce7f2e9.d: src/main.rs
+
+src/main.rs:
---
+++ tutorial/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ianmcnaney/src/rustystuff/tutorial/hello_cargo/target/debug/hello_cargo: /Users/ianmcnaney/src/rustystuff/tutorial/hello_cargo/src/main.rs
